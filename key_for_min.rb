be0a04bc6a6b4c0daf14e12819935b20a6b901e1
DAZUN XING <dazun.xing@gmail.com>
2020-04-03 18:45:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,13 +1,17 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
+
 def key_for_min_value(name_hash)
   if name_hash=={} 
     # really interesting, when using =(i know its worng but pass nil)
     return nil
   else
     a=(name_hash.collect {|x,y| y}).min
-    name_hash.collect {|x,y| return x if y==a}
+    name_hash.collect{ |x,y|
+   
+    return x if y==a}
+    
   end
   
 end
